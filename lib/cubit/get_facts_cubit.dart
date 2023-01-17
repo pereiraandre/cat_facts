09e144c0920def95d37d4fa7a8bff1b6838d94fb
pereiraandre <andre.alvorada@gmail.com>
2023-01-17 19:05:20 +0000

    Taking the variables from cubit out.

--- lib/cubit/get_facts_cubit.dart
+++ lib/cubit/get_facts_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:cat_facts/model/facts_model.dart';
 import 'package:cat_facts/service/cat_facts.dart';
 import 'package:meta/meta.dart';
 
@@ -7,14 +6,12 @@ part 'get_facts_state.dart';
 
 class GetFactsCubit extends Cubit<GetFactsState> {
   GetFactsCubit() : super(GetFactsInitial());
-  FactsService factsService = FactsService();
 
   void getFacts() {
     emit(GetFactsLoading());
     Future.delayed(const Duration(seconds: 3), () async {
       try {
-        final FactsModel? facts = await factsService.getFacts();
-        emit(GetFactsLoaded(catFacts: facts));
+        emit(GetFactsLoaded(catFacts: await FactsService().getFacts()));
       } catch (e) {
         emit(GetFactsError(errorMessage: e.toString()));
       }
